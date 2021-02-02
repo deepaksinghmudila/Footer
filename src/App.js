b321e279538ec0bdc88b202a4f5827fd4d84428d
deepaksinghmudila <deepaksinghmudila21@gmail.com>
2021-02-02 21:22:41 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
- 
+import React from "react";
+import { FooterContainer } from "./containers/footer";
+
+function App() {
+  return (
+    <>
+      <FooterContainer />
+    </>
+  );
+}
+
+export default App;
